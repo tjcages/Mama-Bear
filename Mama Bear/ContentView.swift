11794d006d98da89a527969292500f94b6334a45
Tyler J. Cagle <tylerjcagle@gmail.com>
2020-10-01 11:18:21 -0600

    Initial Commit

---
+++ Mama Bear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mama Bear
+//
+//  Created by Tyler Cagle on 10/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
